0894cb59e6f438f087b93bfd4ab461b17fa378d6
kaushik.ghosh <kaushik.ghosh@intrcity.com>
2023-01-13 02:47:31 +0530

    Runtime polymorphism checking and all functionality working through Model checking in main class

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
-import Model.Issue;
-import Model.SubTask;
-import Model.User;
+import Model.*;
 
 import java.util.ArrayList;
+import java.util.List;
+
 class Main {
   public static void main(String args[]) {
     System.out.println("Hello, world!");
@@ -14,6 +14,24 @@ class Main {
     b.setSubTask(new SubTask(12,123,"Checking Subtask"));
 
     System.out.println(b.getSubTaskList().get(0).getDescription());
+
+    List<SubTask> bug1subtask = new ArrayList<>();
+    Sprint sprint1  = new Sprint(1,"TT_13_01_2022","JANUARY SPRINT",new ArrayList<>());
+    Issue bug1 = new Bug("BUG01", TaskStatus.ASSIGNED, "MWEB PENDING PAGE", "SOME ISSUE FOUND", bug1subtask,sprint1);
+    Issue bug2 = new Bug("BUG02", TaskStatus.ASSIGNED, "DWEB PENDING PAGE", "SOME ISSUE FOUND IN DWEB PAGE", bug1subtask,sprint1);
+    List<Issue> issues = new ArrayList<>();
+    issues.add(bug1);
+    issues.add(bug2);
+    sprint1.setIssues(issues);
+
+
+    System.out.println("Sprint ID:"+sprint1.getId()+"======="+sprint1.getDescription());
+    System.out.println("===============================================");
+
+    for(int i=0;i<sprint1.getIssues().size();i++)
+    {
+      System.out.println(sprint1.getIssues().get(i).getTitle()+"====="+sprint1.getIssues().get(i).getTaskID()+"===="+sprint1.getIssues().get(i).getTaskStatus());
+    }
   
 
   }
